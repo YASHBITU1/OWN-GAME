0b4e2a46b6b64dacd8b016614c01e1f9522e15bc
YASHBITU1 <76907769+YASHBITU1@users.noreply.github.com>
2021-05-08 17:41:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var world;
+var playerIMG;
+var player;
+var bgIMG
+var bg;
+var ground1;
+function preload()
+{
+	playerIMG = loadImage("player2.gif");
+	bgIMG = loadImage("sky.gif")
+
+}
+
+function setup() {
+	createCanvas(700,400);
+ bg =  createSprite(350,200,50,50);
+ bg.scale = 1.55;
+ bg.addImage(bgIMG);
+
+ ground1 = new Ground(200,300,500,15);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	player = createSprite(200,200,50,50);
+	player.addImage(playerIMG);
+    player.scale = 0.1;
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+	background(220)
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
